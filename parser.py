d960dd821fd575c433c8e1ded7ba2560d1617690
Pineapple-Soup <adithyaiyer412@gmail.com>
2024-06-12 14:18:28 -0700

    fix: Corrected file validation, exit on fail

--- parser.py
+++ parser.py
@@ -5,17 +5,17 @@ from pypdf import PdfReader # type: ignore
 def get_input():
     if len(sys.argv) < 2:
         print(f"Usage: python3 {sys.argv[0]} filename")
-        return 1
+        exit(1)
     
-    file = sys.argv[2]
+    file = sys.argv[1]
     if not os.path.isfile(file):
         print(f"{file} is not a valid file")
-        return 1
+        exit(1)
     
     file_name, file_ext = os.path.splitext(file)
-    if file_ext.lower != '.pdf':
+    if file_ext.lower() != '.pdf':
         print(f"{file} must be a PDF file!")
-        return 1
+        exit(1)
     
     return file
     
