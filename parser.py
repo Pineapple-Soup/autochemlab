27ffc0d06cdc276117e3933f220f5525ac9d017f
Pineapple-Soup <adithyaiyer412@gmail.com>
2024-06-22 21:50:21 -0700

    refactor: Use common chemicals CAS API instead of 'chemicals' library to retrieve CAS RNs

--- parser.py
+++ parser.py
@@ -2,7 +2,6 @@ import os
 import re
 import requests # type: ignore
 import sys
-from chemicals import IDs_to_CASs  # type: ignore
 from pypdf import PdfReader # type: ignore
 
 def get_input():
@@ -21,14 +20,50 @@ def get_input():
         exit(1)
     
     return file
-    
+
 def extract_chemical_names(fields : dict):
     return [field.replace("Hazards", "") for field in fields if field.startswith("Hazards")]
 
 def parse_locants(c : str):
     c = re.sub(r'(\d)(\d)', r'\1,\2', c)
-    c = re.sub(r'(\d)([a-zA-Z])', r'\1-\2', c)
+    c = re.sub(r'(\d)([a-zA-Z])|([a-zA-Z])(\d)', r'\1\3-\2\4', c)
+    c = re.sub(r'(\d) ([a-zA-Z])', r'\1-\2', c)
+    # Handle edge cases
+    c = re.sub(r'([a-zA-Z]) (hex+)', r'\1\2', c)
+    c = re.sub("hexanes", "hexane", c)
     return c
+
+def retrieve_CASRN(chemical):
+    try:
+        url = f"https://commonchemistry.cas.org/api/search?q={chemical}"
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        if data['count'] > 0:
+            return data['results'][0]['rn']
+        else:
+            raise ValueError
+    except ValueError as e:
+        return None
+
+def retrieve_all_CASRNs(chemicals):
+    casrns = []
+    for idx, chemical in enumerate(chemicals):
+        retry = 1
+        while retry >= 0:
+            casrn = retrieve_CASRN(chemical)
+            if casrn is not None:
+                casrns.append(casrn)
+                break
+            elif retry > 0:
+                # Option 1: Strip numbers and try again
+                chemical = re.sub(r"^[\d,]+-", "", chemical)
+                chemicals[idx] = chemical
+                retry -= 1
+            else:
+                # Option 2: Prompt user for correct chemical name
+                raise ValueError
+    return casrns
 
 def retrieve_properties(casrn : str):
     compound_data = {}
@@ -56,8 +91,7 @@ if __name__ == "__main__":
     reader = PdfReader(file)
     fields = reader.get_form_text_fields()
     chemical_names = [parse_locants(c) for c in extract_chemical_names(fields)]
-    # TODO: Exception Handling
-    chemical_CASRNs = IDs_to_CASs(chemical_names)
+    chemical_CASRNs = retrieve_all_CASRNs(chemical_names)
     print(chemical_names)
     print(chemical_CASRNs)
     chemical_data = []
@@ -66,6 +100,6 @@ if __name__ == "__main__":
         if properties is not None:
             chemical_data.append({"name" : name , "info" : properties})
     for chemical in chemical_data:
-        print(f"====={chemical["name"]}=====")
+        print(f"\n===== {chemical["name"]} =====")
         for property, value in chemical["info"].items():
             print(f"{property}: {value}")
